9432c4ac7ed09e7bd8471bf3857b8b3232e0ddd6
Luis Escobar <lae311@stern.nyu.edu>
2021-10-27 16:46:09 -0400

    something new

--- index.js
+++ index.js
@@ -5,66 +5,209 @@ const arr = ['AAPL', 'ABT', 'ACN', 'ADBE', 'AMZN', 'ASML', 'AVGO', 'BABA', 'GOOG
 
 function returnTickerData () {
     arr.forEach((e) => {
-    return fetch (`https://financialmodelingprep.com/api/v3/company/discounted-cash-flow/${e}?apikey=dfc166b85e605991cfd559cf91be0d4c`)
-    .then(response => response.json())
-    .then(data => { console.log(data)
-        //create elements
-        const tableRow = document.createElement('tr')
-        const stockPrice = document.createElement('td')
-        const date = document.createElement('td')
-        const dcf = document.createElement('td')
-        const symbol  = document.createElement('td')
-        //fill in elements with data
-        stockPrice.innerHTML = data[ 'Stock Price' ]
-        date.innerHTML = data.date
-        dcf.innerHTML = data.dcf
-        symbol.innerHTML = data.symbol
-        symbol.setAttribute('class', 's')
-        //append the elements
-        tableBody.appendChild(tableRow)
-        tableRow.appendChild(symbol)
-        tableRow.appendChild(stockPrice)
-        tableRow.appendChild(dcf)
-        tableRow.appendChild(date)
-    })})
+        return fetch (`https://financialmodelingprep.com/api/v3/company/discounted-cash-flow/${e}?apikey=dfc166b85e605991cfd559cf91be0d4c`)
+        .then(response => response.json())
+        .then(data => {
+            //create elements
+            const tableRow = document.createElement('tr')
+            const stockPrice = document.createElement('td')
+            const date = document.createElement('td')
+            const dcf = document.createElement('td')
+            const symbol  = document.createElement('td')
+            //fill in elements with data
+            stockPrice.innerHTML = data[ 'Stock Price' ]
+            date.innerHTML = data.date
+            dcf.innerHTML = data.dcf
+            symbol.innerHTML = data.symbol
+            symbol.setAttribute('class', 's')
+            //append the elements
+            tableBody.appendChild(tableRow)
+            tableRow.appendChild(symbol)
+            tableRow.appendChild(stockPrice)
+            tableRow.appendChild(dcf)
+            tableRow.appendChild(date)
+        })})
+    }
+    
+    window.addEventListener('DOMContentLoaded', (event) => {
+        console.log('DOM fully loaded and parsed');
+        returnTickerData()
+    });
+    
+    const balanceBtn = document.getElementById('balance')
+    const cashBtn = document.getElementById('cash')
+    const incomeBtn = document.getElementById('income')
+    
+    incomeBtn.addEventListener('click', () => incomeStatementTableData())
+    
+    const currentTableHead = document.getElementById('table-head')
+    const currentTableHeadRow = document.getElementById('table-head-row')
+    const table = document.getElementById('table')
+
+    function incomeStatementTableData () {
+        //create table elements
+        currentTableHeadRow.remove()
+        const newTableRow = document.createElement('tr')
+        const y = currentTableHead.appendChild(newTableRow)
+        //create table header info
+        fetch(`https://financialmodelingprep.com/api/v3/income-statement/BABA?apikey=dfc166b85e605991cfd559cf91be0d4c`)
+        .then(response => response.json())
+        .then(data => {
+            dataAtZeroHead= data[0]
+            let inc=0
+            for (const [key] of Object.entries(dataAtZeroHead)) {
+                if (inc < 50) {
+                    inc++;
+                    let newTableHeadContent = document.createElement('th')
+                    newTableHeadContent.innerHTML = `${key}`;
+                    y.appendChild(newTableHeadContent)
+                }
+            }})
+            //save ticker symbols into new array so we can delete old data
+            const symbols = [...document.getElementsByClassName('s')]
+            const x = symbols.map(function(item){
+                const y = item.textContent
+                return y
+                })
+            console.log(x)
+            tableBody.remove()
+            const newTableBody = document.createElement('tbody')
+            table.appendChild(newTableBody)
+            arr.forEach(function(ticker) {
+                fetch(`https://financialmodelingprep.com/api/v3/income-statement/${ticker}?apikey=dfc166b85e605991cfd559cf91be0d4c`)
+                .then(resp => resp.json())
+                .then(d => {
+                    //accessing the first object with all the data
+            dataAtZero= d[0]
+            let i = 0
+            const r = document.createElement('tr')
+            newTableBody.appendChild(r)
+            for (const [key, value] of Object.entries(dataAtZero)) {
+                console.log(value)
+                if (i < 50) {
+                    i++;
+                    let newTableData = document.createElement('td')
+                    newTableData.innerHTML = `${value}`;
+                    r.appendChild(newTableData)
+                }
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    })
 }
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    returnTickerData()
-});
-
-const practice = document.getElementById('practice')
-
-practice.addEventListener('click', () => incomeStatementTableData())
-function incomeStatementTableData () {
-    const symbols = [...document.getElementsByClassName('s')]
-    symbols.forEach(function(item){
-        const x = item.textContent 
-        console.log(x)
-        const tableRow = document.createElement('tr')
-        fetch(`https://financialmodelingprep.com/api/v3/income-statement/${x.toUpperCase()}?limit=120apikey=dfc166b85e605991cfd559cf91be0d4c`)
-                        .then(response => console.log(response.json()))
-                        .then(data => {
-                            //accessing the first object with all the data
-                            const dataAtZero = data[0]
-                            let i = 0
-                            for (const [key, value] of Object.entries(dataAtZero)) {
-                                let ele = document.createElement('p')
-                                ele.setAttribute('class', 'key-metrics')
-                                if ([value] === null) {
-                                    console.log('hello')
-                                }
-                                else if ([value] > 0 &&  i< 3) {
-                                    i++;
-                                    ele.innerHTML = `${key} : ${value}`;
-                                    boxUno.appendChild(ele)} }
-                                    })
-                        .catch((error) => {
-                        console.log(error);
-                    })
-                    })
+balanceBtn.addEventListener('click', () => balanceStatementTableData())
+    function balanceStatementTableData () {
+        //create table elements
+        currentTableHeadRow.remove()
+        const newTableRow = document.createElement('tr')
+        const y = currentTableHead.appendChild(newTableRow)
+        //create table header info
+        fetch(`https://financialmodelingprep.com/api/v3/balance-sheet-statement/AAPL?limit=120&apikey=dfc166b85e605991cfd559cf91be0d4c`)
+        .then(response => response.json())
+        .then(data => {
+            dataAtZeroHead= data[0]
+            let inc=0
+            for (const [key] of Object.entries(dataAtZeroHead)) {
+                if (inc < 50) {
+                    inc++;
+                    let newTableHeadContent = document.createElement('th')
+                    newTableHeadContent.innerHTML = `${key}`;
+                    y.appendChild(newTableHeadContent)
                 }
+            }})
+            //save ticker symbols into new array so we can delete old data
+            const symbols = [...document.getElementsByClassName('s')]
+            const x = symbols.map(function(item){
+                const y = item.textContent
+                return y
+                })
+            console.log(x)
+            tableBody.remove()
+            const newTableBody = document.createElement('tbody')
+            table.appendChild(newTableBody)
+            arr.forEach(function(ticker) {
+                fetch(`https://financialmodelingprep.com/api/v3/balance-sheet-statement/${ticker}?apikey=dfc166b85e605991cfd559cf91be0d4c`)
+                .then(resp => resp.json())
+                .then(d => {
+                    //accessing the first object with all the data
+            dataAtZero= d[0]
+            let i = 0
+            const r = document.createElement('tr')
+            newTableBody.appendChild(r)
+            for (const [key, value] of Object.entries(dataAtZero)) {
+                console.log(value)
+                if (i < 50) {
+                    i++;
+                    let newTableData = document.createElement('td')
+                    newTableData.innerHTML = `${value}`;
+                    r.appendChild(newTableData)
+                }
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    })
+}
+
+cashBtn.addEventListener('click', () => cashStatementTableData())
+    function cashStatementTableData () {
+        //create table elements
+        currentTableHeadRow.remove()
+        const newTableRow = document.createElement('tr')
+        const y = currentTableHead.appendChild(newTableRow)
+        //create table header info
+        fetch(`https://financialmodelingprep.com/api/v3/cash-flow-statement/AAPL?limit=120&apikey=dfc166b85e605991cfd559cf91be0d4c`)
+        .then(response => response.json())
+        .then(data => {
+            dataAtZeroHead= data[0]
+            let inc=0
+            for (const [key] of Object.entries(dataAtZeroHead)) {
+                if (inc < 50) {
+                    inc++;
+                    let newTableHeadContent = document.createElement('th')
+                    newTableHeadContent.innerHTML = `${key}`;
+                    y.appendChild(newTableHeadContent)
+                }
+            }})
+            //save ticker symbols into new array so we can delete old data
+            const symbols = [...document.getElementsByClassName('s')]
+            const x = symbols.map(function(item){
+                const y = item.textContent
+                return y
+                })
+            console.log(x)
+            tableBody.remove()
+            const newTableBody = document.createElement('tbody')
+            table.appendChild(newTableBody)
+            arr.forEach(function(ticker) {
+                fetch(`https://financialmodelingprep.com/api/v3/cash-flow-statement/${ticker}?apikey=dfc166b85e605991cfd559cf91be0d4c`)
+                .then(resp => resp.json())
+                .then(d => {
+                    //accessing the first object with all the data
+            dataAtZero= d[0]
+            let i = 0
+            const r = document.createElement('tr')
+            newTableBody.appendChild(r)
+            for (const [key, value] of Object.entries(dataAtZero)) {
+                console.log(value)
+                if (i < 50) {
+                    i++;
+                    let newTableData = document.createElement('td')
+                    newTableData.innerHTML = `${value}`;
+                    r.appendChild(newTableData)
+                }
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    })
+}
+
 
 
 
@@ -75,11 +218,10 @@ function incomeStatementTableData () {
     form.addEventListener ('submit', (e) =>{
         e.preventDefault();
         stockName = search.value
-        fetch (`https://financialmodelingprep.com/api/v3/company/discounted-cash-flow/${stockName.toUpperCase()}?apikey=97e3e99e10b8a54d8832bd62a5440ded`)
+        fetch (`https://financialmodelingprep.com/api/v3/company/discounted-cash-flow/${stockName.toUpperCase()}?apikey=dfc166b85e605991cfd559cf91be0d4c`)
         .then(response => {
             if (response.ok) {
                 response.json()
-
                 .then(data => {
         //create elements
                 const tableRow = document.createElement('tr')
@@ -100,11 +242,13 @@ function incomeStatementTableData () {
                 tableRow.appendChild(dcf)
                 tableRow.appendChild(date)
                 //looping through the  classesafter the event listener of adding a stock
-                const symbols = [...document.getElementsByClassName('s')].forEach(function(item) {
-                    //item here are the classes
-                    //everytime we click on the classes
-                    item.addEventListener('click', function() {
-                    fetch(`https://financialmodelingprep.com/api/v3/key-metrics-ttm/${stockName.toUpperCase()}?apikey=97e3e99e10b8a54d8832bd62a5440ded`)
+                const symbols = [...document.getElementsByClassName('s')]
+                console.log(symbols)
+                symbols.forEach(function(item) {
+                    item.addEventListener('click', function (e){
+                        const x = item.innerHTML
+                        console.log(e)
+                    fetch(`https://financialmodelingprep.com/api/v3/key-metrics-ttm/${x}?limit=40&apikey=dfc166b85e605991cfd559cf91be0d4c`)
                     .then(response => response.json())
                     .then(data => {
                         //accessing the first object with all the data
@@ -113,19 +257,20 @@ function incomeStatementTableData () {
                         for (const [key, value] of Object.entries(dataAtZero)) {
                             let ele = document.createElement('p')
                             ele.setAttribute('class', 'key-metrics')
-                            if ([value] === null) {
-                                console.log('hello')
-                            }
-                            else if ([value] > 0 &&  i< 3) {
+                            if (value > 0 &&  i < 3) {
                                 i++;
                                 ele.innerHTML = `${key} : ${value}`;
-                                boxUno.appendChild(ele)} }
-                                })
+                                boxUno.appendChild(ele)
+                                console.log(ele)
+                            } 
+                            else if ( value === null) {
+                                console.log('hello')
+                            }
+                        }
+                    })
                             })
                         })
                     })
-
-                    // const p = document.createElement('p')
                 }})
                 
             .catch((error) => {
@@ -134,6 +279,84 @@ function incomeStatementTableData () {
     form.reset()
         })
 
+
+const searchUno = document.getElementById('stock1Search')
+const searchDos = document.getElementById('stock2Search')
+const inputUno = document.getElementById('search1')
+const inputDos = document.getElementById('search2')
+
+searchUno.addEventListener('submit', (e) => {
+    e.preventDefault()
+    x = inputUno.value
+    console.log(x)
+    fetch(`https://financialmodelingprep.com/api/v3/key-metrics-ttm/${x.toUpperCase()}?limit=40&apikey=dfc166b85e605991cfd559cf91be0d4c`)
+                    .then(response => response.json())
+                    .then(data => {
+                        //accessing the first object with all the data
+                        const dataAtZero = data[0]
+                        let i = 0
+                        for (const [key, value] of Object.entries(dataAtZero)) {
+                            let ele = document.createElement('p')
+                            ele.setAttribute('class', 'key-metrics-1')
+                            if (value > 0 &&  i < 3) {
+                                i++;
+                                ele.innerHTML = `${key} : ${value}`;
+                                boxUno.appendChild(ele)
+                                console.log(ele)
+                            } 
+                            else if ( value === null) {
+                                console.log('hello')
+                            }
+                        }
+                    })
+                
+            .catch((error) => {
+                console.log(error);
+            })
+    form.reset()
+})
+searchDos.addEventListener('submit', (e) => {
+    e.preventDefault()
+    x = inputUno.value
+    console.log(x)
+    fetch(`https://financialmodelingprep.com/api/v3/key-metrics-ttm/${x.toUpperCase()}?limit=40&apikey=dfc166b85e605991cfd559cf91be0d4c`)
+                    .then(response => response.json())
+                    .then(data => {
+                        //accessing the first object with all the data
+                        const dataAtZero = data[0]
+                        let i = 0
+                        for (const [key, value] of Object.entries(dataAtZero)) {
+                            let ele = document.createElement('p')
+                            ele.setAttribute('class', 'key-metrics-2')
+                            if (value > 0 &&  i < 3) {
+                                i++;
+                                ele.innerHTML = `${key} : ${value}`;
+                                boxDos.appendChild(ele)
+                                console.log(ele)
+                            } 
+                            else if ( value === null) {
+                                console.log('hello')
+                            }
+                        }
+                    })
+                
+            .catch((error) => {
+                console.log(error);
+            })
+    form.reset()
+})
+
+const compareBtn = document.getElementById('compare')
+
+compareBtn.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const keyMetricsDos = [...document.getElementsByClassName('key-metrics-2')]
+    const keyMetricsUno = [...document.getElementsByClassName('key-metrics-1')]
+    const uno = keyMetricsUno.map(function(item) {
+        return item.textContent
+    })
+    console.log(uno)
+})
     
 
 
